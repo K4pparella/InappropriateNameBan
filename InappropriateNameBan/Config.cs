dd4f50cf90f32a7b9878ed3042a26622a1ba8f6d
Clown <65295432+SpaceTm@users.noreply.github.com>
2022-01-15 16:27:09 +0100

    Config now clean

--- InappropriateNameBan/Config.cs
+++ InappropriateNameBan/Config.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Exiled.API.Interfaces;
 
-namespace Testasdadas
+namespace InappropriateNameBan
 {
     public class Config : IConfig
     {
         [Description("Is the plugin enabled?")]
         public bool IsEnabled { get; set; } = true;
 
-        [Description("The list that is used to store all of the bad names")]
+        [Description("Should debug message be enabled?")]
+        public bool Debug { get; set; } = false;
+
+        [Description("The list that is used to store all of the inappropriate names")]
         public List<string> BannedNames { get; set; } = new List<string>
         {
-            "BOSSETTI"
+            "Fuck",
+            "Dick",
+            "YourBannedName"
         };
 
-        [Description("The list of the names that will change the bad name (will pick in a random order)")]
+        [Description("The list of the names that will change the inappropriate name (will pick in a random order) (leave one name to use that one)")]
         public List<string> ChangeNames { get; set; } = new List<string> 
         {
-            "CENSURATO"
+            "Censored"
         };
     }
 }
