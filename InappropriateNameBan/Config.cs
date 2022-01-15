db04282bc0f524c29320370bc7f658384ffc070a
Clown <65295432+SpaceTm@users.noreply.github.com>
2022-01-15 16:08:28 +0100

    Add files via upload

---
+++ InappropriateNameBan/Config.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace Testasdadas
+{
+    public class Config : IConfig
+    {
+        [Description("Is the plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("The list that is used to store all of the bad names")]
+        public List<string> BannedNames { get; set; } = new List<string>
+        {
+            "BOSSETTI"
+        };
+
+        [Description("The list of the names that will change the bad name (will pick in a random order)")]
+        public List<string> ChangeNames { get; set; } = new List<string> 
+        {
+            "CENSURATO"
+        };
+    }
+}
---
+++ InappropriateNameBan/EventHandlers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace Testasdadas
+{
+    public class EventHandlers
+    {
+        Random random = Exiled.Loader.Loader.Random;
+        public void OnPlayerJoining(VerifiedEventArgs ev)
+        {
+            int ListCount;
+            bool contained;
+
+            ListCount = Plugin.Singleton.Config.BannedNames.Count;
+            
+            for(int i = 0; i < ListCount; i++)
+            {
+                contained = ev.Player.Nickname.Contains(Plugin.Singleton.Config.BannedNames[i]);
+                if(contained == true)
+                {
+                    Log.Debug("Attention, a player with a inappropriate name has been found, replacing name...");
+                    string[] newnames = Plugin.Singleton.Config.ChangeNames.ToArray();
+                    int rn = random.Next(0, newnames.Length);
+                    ev.Player.DisplayNickname = Plugin.Singleton.Config.ChangeNames[rn];
+                }
+            }
+        }
+    }
+}
