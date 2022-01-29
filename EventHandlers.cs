fdf08874bb6d9c9bd4315ffa9afbfd3833dc2b57
Rin <65295432+quazEx@users.noreply.github.com>
2022-01-29 18:35:39 +0100

    Add files via upload

---
+++ EventHandlers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace InappropriateNameBan
+{
+    public class EventHandlers
+    {
+        Random random = Exiled.Loader.Loader.Random;
+        public void OnPlayerJoining(VerifiedEventArgs ev)
+        {
+            int ListCount;
+            bool contained;
+            bool inappropriatefound = false;
+            int i = 0;
+
+            ListCount = Plugin.Singleton.Config.BannedNames.Count;
+
+            while (i < ListCount && inappropriatefound == false)
+            {
+                contained = ev.Player.Nickname.Contains(Plugin.Singleton.Config.BannedNames[i]);
+                if (contained == true)
+                {
+                    string[] newnames = Plugin.Singleton.Config.ChangeNames.ToArray();
+                    int rn = random.Next(0, newnames.Length);
+                    ev.Player.DisplayNickname = Plugin.Singleton.Config.ChangeNames[rn];
+                    inappropriatefound = true;
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using Exiled.API.Features;
+using System;
+using Player = Exiled.Events.Handlers.Player;
+using Map = Exiled.Events.Handlers.Map;
+using Server = Exiled.Events.Handlers.Server;
+
+namespace InappropriateNameBan
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "quazEx";
+
+        public override string Name => "InappropriateNameBan";
+
+        public override string Prefix => "InappropriateNameBan";
+
+        public override Version Version => new Version(1, 5, 0);
+
+        public override Version RequiredExiledVersion => new Version(4, 2, 2);
+
+        internal EventHandlers EventHandlers { get; set; }
+
+        public override void OnEnabled()
+        {
+            EventHandlers = new EventHandlers();
+            Player.Verified += EventHandlers.OnPlayerJoining;
+            Singleton = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Verified -= EventHandlers.OnPlayerJoining;
+            Singleton = null;
+            base.OnDisabled();
+        }
+
+        public static Plugin Singleton;
+
+    }
+}
